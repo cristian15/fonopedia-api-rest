3b11db81f70ec717989dcc5ffa9d2d3320b42299
Cristian Beltran <cbeltranc6@gmail.com>
2018-12-12 14:30:03 -0300

    Patologia route

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -5,6 +5,7 @@ const usuarioRoutes = require('./usuario.routes');
 const loginRoutes = require('./login.routes');
 const uploadsRoutes = require('./upload.routes');
 const emailRoutes = require('./email.routes');
+const patologiasRoutes = require('./patologias.routes');
 
 
 let app = express();
@@ -13,6 +14,7 @@ app.use('/uploads', uploadsRoutes);
 app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
 app.use('/email', emailRoutes);
+app.use('/patologias', patologiasRoutes);
 app.use('/', appRoutes);
 
 module.exports = app;
