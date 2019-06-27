e0bc4337f7fbb7214c9e8266a61a2da6dae3be59
Cristian Beltran <cbeltranc6@gmail.com>
2019-06-27 11:07:02 -0400

    API en Produccion

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 // Importar rutas
 const apiRoutes = require('./routes/api.routes');
-server.app.use('/', express.static('cliente'));
+app.use('/', express.static('cliente'));
 
 
 // Conexion DB
